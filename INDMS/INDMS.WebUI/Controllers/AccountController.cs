e2ec2f8e05af0b84e2e0a34cacb5077146c05604
atmiya <atmiya@live.com>
2016-07-26 12:24:48 +0530

    201607261224

--- INDMS/INDMS.WebUI/Controllers/AccountController.cs
+++ INDMS/INDMS.WebUI/Controllers/AccountController.cs
@@ -6,55 +6,77 @@ using System.Configuration;
 using System.Linq;
 using System.Web.Mvc;
 
-namespace INDMS.WebUI.Controllers {
+namespace INDMS.WebUI.Controllers
+{
 
-    public class AccountController : Controller {
+    public class AccountController : Controller
+    {
         private INDMSEntities db = new INDMSEntities();
 
         // GET: Account
-        public ActionResult Login() {
+        public ActionResult Login()
+        {
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Login(User user, string returnUrl) {
-            if (ModelState.IsValid) {
+        public ActionResult Login(User user, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
                 string userName = user.UserName;
                 string password = System.Text.Encoding.ASCII.EncodeBase64(user.Password);
-                if (user.UserName.Equals(ConfigurationManager.AppSettings["UserName"].ToString()) && user.Password.Equals(ConfigurationManager.AppSettings["Password"].ToString())) {
+                if (user.UserName.Equals(ConfigurationManager.AppSettings["UserName"].ToString()) && user.Password.Equals(ConfigurationManager.AppSettings["Password"].ToString()))
+                {
                     CreateSession(user);
-                    if (!string.IsNullOrEmpty(returnUrl)) {
+                    if (!string.IsNullOrEmpty(returnUrl))
+                    {
                         return Redirect(returnUrl);
                     }
-                    else {
+                    else
+                    {
                         return RedirectToAction("Dashboard", "Home");
                     }
                 }
-                else {
-                    User _user = db.Users.Where(i => i.UserName == userName && i.Password == password && i.Active != "N").SingleOrDefault();
+                else
+                {
+                    try
+                    {
+                        db.Database.Connection.Open();
+                        User _user = db.Users.Where(i => i.UserName == userName && i.Password == password && i.Active != "N").SingleOrDefault();
 
-                    if (_user != null) {
-                        CreateSession(_user);
-                        if (!string.IsNullOrEmpty(returnUrl)) {
-                            return Redirect(returnUrl);
-                        }
-                        else {
-                            return RedirectToAction("Dashboard", "Home");
+                        if (_user != null)
+                        {
+                            CreateSession(_user);
+                            if (!string.IsNullOrEmpty(returnUrl))
+                            {
+                                return Redirect(returnUrl);
+                            }
+                            else
+                            {
+                                return RedirectToAction("Dashboard", "Home");
+                            }
                         }
                     }
+                    catch(Exception ex)
+                    {
+                        Response.Write(ex.StackTrace);
+                    } 
                 }
             }
 
             return View();
         }
 
-        public ActionResult Logout() {
+        public ActionResult Logout()
+        {
             Response.Cookies["INDMS"].Expires = DateTime.Now.AddDays(-1);
             return RedirectToAction("Login");
         }
 
-        private void CreateSession(User user) {
+        private void CreateSession(User user)
+        {
             Response.Cookies["INDMS"]["UserID"] = user.UserId.ToString();
             Response.Cookies["INDMS"]["UserName"] = System.Text.Encoding.ASCII.EncodeBase64(user.UserName);
             Response.Cookies["INDMS"]["Name"] = System.Text.Encoding.ASCII.EncodeBase64(user.Name);
@@ -63,7 +85,8 @@ namespace INDMS.WebUI.Controllers {
         }
 
         [HttpPost]
-        public ActionResult GetJsonObjOfRoles() {
+        public ActionResult GetJsonObjOfRoles()
+        {
             IEnumerable<string> KeyValueList = db.Roles.Select(x => x.Role1).ToList<string>();
 
             return Json(KeyValueList, JsonRequestBehavior.AllowGet);
